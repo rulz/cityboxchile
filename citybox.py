c147814f38b8d6a60c6f776852aad096fc211e15
Rulz <raul@witoi.com>
2014-04-02 13:46:52 -0300

    get all citybox of santiago de chile

---
+++ citybox.py
@@ -0,0 +1,21 @@
+import urllib2
+import json
+import xmltodict
+
+url = 'http://api-cl.easypack24.net/?do=listmachines_xml'
+
+
+def get_dict_citybox(url=url):
+    file = urllib2.urlopen(url)
+    data = file.read()
+    file.close()
+    string = xmltodict.parse(data)
+    dict = json.loads(json.dumps(string, ensure_ascii=False))['paczkomaty']['machine']
+    return dict
+
+
+def get_choices_citybox():
+    data = get_dict_citybox()
+    CHOICES_CITYBOX = [('citybox: ' + citybox['street'] + ' ' + citybox['buildingnumber'] + ', ' + citybox['province'], citybox['locationdescription'])
+                       for citybox in data]
+    return CHOICES_CITYBOX
